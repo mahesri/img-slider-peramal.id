95ceeac04d438bd1ede033221fa68889aaf6ef2c
unknown <adi504097@gmail.com>
2023-07-23 12:44:25 +0700

    add arrow function

--- js.js
+++ js.js
@@ -1,7 +1,26 @@
 const carousel = document.querySelector(".carousel");
 
-const dragging = (e) => {
-   console.log(e.pageX);
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const DragStart = (e) => {
+    // Updatating global variable value on mouse dwon event
+    isDragStart = true;
+    prevPageX = e.pageX
+    prevScrollLeft = carousel.scrollLeft;
 }
 
+const dragging = (e) => {
+    // Scrolling images/carousel to left according to mouse pointer
+    if(isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", DragStart);
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
