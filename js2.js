f93c3d729da4a267cda99250cfcf2fbf3431ffc3
unknown <adi504097@gmail.com>
2023-07-27 14:30:19 +0700

    add scrollable script

---
+++ js2.js
@@ -0,0 +1,65 @@
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carousel.querySelector(".card").clienttWidth;
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, startX, startScrollLeft, timeoutId;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll postion of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; //if isDragging is false return fom here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const autoPlay = () => {
+        if(window.innerWidth < 800 ) return;
+        timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 25);
+}
+
+autoPlay();
+
+const infiniteScroll = () => {
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+
+    } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+      carousel.scrollLeft = carousel.offsetWidth;
+      carousel.classList.remove("no-transition");
+    }
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
